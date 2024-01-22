a6645bf58e59947803ae39b5ac07573ddf81db8a
Oscare101 <kirillfedortsev2000@gmail.com>
2024-01-22 22:54:43 +0200

    test eas with mmkv

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { MMKV } from 'react-native-mmkv'
+
+const storage = new MMKV()
 
 export default function App() {
+  const [count, setCount] = useState<number>(0)
+
+  useEffect(() => {
+    const key = storage.getNumber('key')
+    if (key) {
+      setCount(key)
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{count}</Text>
       <StatusBar style="auto" />
+
+      <Button
+        title="+1"
+        onPress={() => {
+          const newCount = count + 1
+          setCount(newCount)
+          storage.set('key', newCount)
+        }}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +39,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
