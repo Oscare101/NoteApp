ce450498f6df263d03be06f6271b9d62e246d019
Oscare101 <kirillfedortsev2000@gmail.com>
2024-01-23 12:50:12 +0200

    colors

---
+++ constants/colors.ts
@@ -0,0 +1,14 @@
+export default {
+  light: {
+    main: '#050D16',
+    bg: '#FFFDFC',
+    card: '#FFF7E6',
+    button: '#FFE4B0',
+    successMain: '#51AB32',
+    successBg: '#CBE6C2',
+    warningMain: '#EAD40A',
+    warningBg: '#F1EBB3',
+    errorMain: '#FF4843',
+    errorBg: '#FFE0E1',
+  },
+}
